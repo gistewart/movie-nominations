84f4eb0ce73d1e8a3e7788fee3e4e512c09c9ebc
Graeme Stewart <gi_stewart@yahoo.com>
2021-05-06 16:16:50 -0400

    testing deployment build

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,13 +4,13 @@ const apiKey = process.env.REACT_APP_OMDB_API_KEY;
 export default {
   getSearchByName: function (query) {
     return axios.get(
-      `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}*&type=movie&page=1`
+      `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}*&type=movie&page=1`
     );
   },
 
   getSearchByID: function (query) {
     return axios.get(
-      `http://www.omdbapi.com/?apikey=${apiKey}&i=${query}&type=movie&page=1`
+      `https://www.omdbapi.com/?apikey=${apiKey}&i=${query}&type=movie&page=1`
     );
   },
 };
